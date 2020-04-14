089efb7cf4ba7cb57ca42d30d7aaeb7416144e82
sdadas11 <61620491+sdadas11@users.noreply.github.com>
2020-04-14 12:19:48 +0800

    Add files via upload

---
+++ CrawlerdoubanApplication.java
@@ -0,0 +1,13 @@
+package com.mcy.crawlerdouban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerdoubanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlerdoubanApplication.class, args);
+    }
+
+}
---
+++ crawlerdouban/src/test/java/com/mcy/crawlerdouban/CrawlerdoubanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcy.crawlerdouban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlerdoubanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
